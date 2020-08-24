83008eebcd739fec890cc39ad9795b8479b21399
Nick Stenquist <whiteshadow148@gmail.com>
2020-08-23 20:11:43 -0600

    Added button click
    video 5

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 class MyGrid(Widget):
-    pass
+    name = ObjectProperty(None)
+    email = ObjectProperty(None)
+
+    def btn(self):
+        print("Name:", self.name.text, "Email:", self.email.text)
+        self.name.text = ""
+        self.email.text = ""
     # def __init__(self, **kwargs):
     #     super(MyGrid, self).__init__(**kwargs)
     #     self.cols = 1
