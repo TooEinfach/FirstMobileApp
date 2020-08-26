089d381edeb188fefa3ea13a2d06b5047ba7f6b8
Nick Stenquist <whiteshadow148@gmail.com>
2020-08-25 18:50:10 -0600

    Drawing rectangle that moves with click and draw line
    video 8

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from kivy.uix.floatlayout import FloatLayout
 # from kivy.uix.gridlayout import GridLayout
 # from kivy.uix.textinput import TextInput
 # from kivy.uix.button import Button
-# from kivy.uix.widget import Widget
-# from kivy.properties import ObjectProperty
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.graphics import Rectangle
+from kivy.graphics import Color
+from kivy.graphics import Line
 
 # class MyGrid(Widget):
     # name = ObjectProperty(None)
@@ -51,11 +54,36 @@ from kivy.uix.floatlayout import FloatLayout
     #     self.lastName.text = ""
     #     self.email.text = ""
 
+class Touch(Widget):
+    def __init__(self, **kwargs):
+        super(Touch, self).__init__(**kwargs)
+
+        with self.canvas:
+            Color(1, 0, 5, .5, mode='rgba')
+            Line(points=(20, 30, 400, 500, 60, 30))
+            Color(1, 0, 0, .5, mode='rgba')
+            self.rect = Rectangle(pos=(0,0), size=(50, 50))
+            # Color(1, 1, 0, .5, mode='rgba')
+            # self.rect = Rectangle(pos=(200,300), size=(150, 50))
+
+
+
+    # btn = ObjectProperty(None)
+    def on_touch_down(self, touch):
+        self.rect.pos = touch.pos
+        print("Mouse Down", touch)
+        # self.btn.opacity = 0.5
+    def on_touch_move(self, touch):
+        self.rect.pos = touch.pos
+        print("Mouse Move", touch)
+    # def on_touch_up(self, touch):
+    #     self.btn.opacity = 1
 
 class MyApp (App):
     def build(self):
         # return MyGrid()
-        return FloatLayout()
+        # return FloatLayout()
+        return Touch()
 
 if __name__ == "__main__":
     MyApp().run()
