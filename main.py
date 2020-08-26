2548d64e4b4b222a5409204e530f1f13a9e058a9
Nick Stenquist <whiteshadow148@gmail.com>
2020-08-26 09:20:47 -0600

    Created multiple windows and transitions between those windows
    video 9

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import kivy
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+# from kivy.uix.floatlayout import FloatLayout
 # from kivy.uix.label import Label
 # from kivy.uix.gridlayout import GridLayout
 # from kivy.uix.textinput import TextInput
 # from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
-from kivy.graphics import Rectangle
-from kivy.graphics import Color
-from kivy.graphics import Line
+# from kivy.uix.widget import Widget
+# from kivy.properties import ObjectProperty
+# from kivy.graphics import Rectangle
+# from kivy.graphics import Color
+# from kivy.graphics import Line
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class MainWindow(Screen):
+    pass
+class SecondWindow(Screen):
+    pass
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file("my.kv")
 
 # class MyGrid(Widget):
     # name = ObjectProperty(None)
@@ -54,36 +65,36 @@ from kivy.graphics import Line
     #     self.lastName.text = ""
     #     self.email.text = ""
 
-class Touch(Widget):
-    def __init__(self, **kwargs):
-        super(Touch, self).__init__(**kwargs)
+# class Touch(Widget):
+#     def __init__(self, **kwargs):
+#         super(Touch, self).__init__(**kwargs)
 
-        with self.canvas:
-            Color(1, 0, 5, .5, mode='rgba')
-            Line(points=(20, 30, 400, 500, 60, 30))
-            Color(1, 0, 0, .5, mode='rgba')
-            self.rect = Rectangle(pos=(0,0), size=(50, 50))
+#         with self.canvas:
+#             Color(1, 0, 5, .5, mode='rgba')
+#             Line(points=(20, 30, 400, 500, 60, 30))
+#             Color(1, 0, 0, .5, mode='rgba')
+#             self.rect = Rectangle(pos=(0,0), size=(50, 50))
             # Color(1, 1, 0, .5, mode='rgba')
             # self.rect = Rectangle(pos=(200,300), size=(150, 50))
 
 
 
     # btn = ObjectProperty(None)
-    def on_touch_down(self, touch):
-        self.rect.pos = touch.pos
-        print("Mouse Down", touch)
-        # self.btn.opacity = 0.5
-    def on_touch_move(self, touch):
-        self.rect.pos = touch.pos
-        print("Mouse Move", touch)
+    # def on_touch_down(self, touch):
+    #     self.rect.pos = touch.pos
+    #     print("Mouse Down", touch)
+    #     # self.btn.opacity = 0.5
+    # def on_touch_move(self, touch):
+    #     self.rect.pos = touch.pos
+    #     print("Mouse Move", touch)
     # def on_touch_up(self, touch):
     #     self.btn.opacity = 1
 
-class MyApp (App):
+class MyMainApp (App):
     def build(self):
         # return MyGrid()
         # return FloatLayout()
-        return Touch()
+        return kv
 
 if __name__ == "__main__":
-    MyApp().run()
+    MyMainApp().run()
